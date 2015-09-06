8b35e6a4e354f755e86a21b30645a2e72e78f706
mehrad amin eskandari <mehradae@gmail.com>
2015-09-06 08:39:32 +0000

    token

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 import webapp2
 
-TOKEN = 'YOUR_BOT_TOKEN_HERE'
+TOKEN = '114551407:AAHlMtt4VgB3aJVuj6wfrgDRZKMzXs6tcho'
 
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
 
